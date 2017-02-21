a2fefaf7d8c82748fdb037ea2c7d41410c42c3a7
Brianna <briannaburrows@gmail.com>
2017-02-20 17:02:05 -0800

    Adding instances of object ImageProducts

--- app.js
+++ app.js
@@ -20,24 +20,48 @@ ImageProducts.prototype.getTimesDisplayed = function () {
 
 };
 
-var bagImage = new ImageProducts ('bagImage', 'C3PO Luggage Bag', './assets/bag.jpg');
-var bananaImage = new ImageProducts ('banImage', 'Banana Cutter', './assets/banana.jpg');
-var bathroomImage = new ImageProducts ('bathroomImage', 'iPad and Toilet Roll Holder', './assets/bathroom.jpg');
-var bootsImage = new ImageProducts ('bootsImage', 'Toeless Boots', './assets/boots.jpg');
-var breakfastImage = new ImageProducts ('breakfastImage', 'The Ultimate Breakfast Maker', './assets/breakfast.jpg');
-var bagImage = new ImageProducts ('bagImage', 'C3PO Luggage Bag', './assets/bag.jpg');
-var bagImage = new ImageProducts ('bagImage', 'C3PO Luggage Bag', './assets/bag.jpg');
-var bagImage = new ImageProducts ('bagImage', 'C3PO Luggage Bag', './assets/bag.jpg');
-var bagImage = new ImageProducts ('bagImage', 'C3PO Luggage Bag', './assets/bag.jpg');
-var bagImage = new ImageProducts ('bagImage', 'C3PO Luggage Bag', './assets/bag.jpg');
-var bagImage = new ImageProducts ('bagImage', 'C3PO Luggage Bag', './assets/bag.jpg');
-var bagImage = new ImageProducts ('bagImage', 'C3PO Luggage Bag', './assets/bag.jpg');
-var bagImage = new ImageProducts ('bagImage', 'C3PO Luggage Bag', './assets/bag.jpg');
-var bagImage = new ImageProducts ('bagImage', 'C3PO Luggage Bag', './assets/bag.jpg');
-var bagImage = new ImageProducts ('bagImage', 'C3PO Luggage Bag', './assets/bag.jpg');
-var bagImage = new ImageProducts ('bagImage', 'C3PO Luggage Bag', './assets/bag.jpg');
-var bagImage = new ImageProducts ('bagImage', 'C3PO Luggage Bag', './assets/bag.jpg');
-var bagImage = new ImageProducts ('bagImage', 'C3PO Luggage Bag', './assets/bag.jpg');
-var bagImage = new ImageProducts ('bagImage', 'C3PO Luggage Bag', './assets/bag.jpg');
-var bagImage = new ImageProducts ('bagImage', 'C3PO Luggage Bag', './assets/bag.jpg');
-var bagImage = new ImageProducts ('bagImage', 'C3PO Luggage Bag', './assets/bag.jpg');
+var catalogEntryOne = new ImageProducts('bag', 'C3PO Luggage Bag', 'assets/bag.jpg');
+
+var catalogEntryTwo = new ImageProducts('banana', 'Banana Slicer', 'assets/banana.jpg');
+
+var catalogEntryThree = new ImageProducts('bathroom', 'iPad and Toilet Roll Holder', 'assets/bathroom.jpg');
+
+var catalogEntryFour = new ImageProducts('boots', 'Toeless Boots','assets/boots.jpg');
+
+var catalogEntryFive = new ImageProducts('breakfast', 'The Ultimate Breakfast Maker','assets/breakfast.jpg');
+
+var catalogEntrySix = new ImageProducts('bubblegum', 'Meatball Bubblegum', 'assets/bubblegum.jpg');
+
+var catalogEntrySeven = new ImageProducts('chair', 'Red Chair','assets/chair.jpg');
+
+var catalogEntryEight = new ImageProducts('cthulhu', 'Green Creature Figurine','assets/cthulhu.jpg');
+
+var catalogEntryNine = new ImageProducts('dog-duck', 'Dog Duck Muzzel','assets/dog-duck.jpg');
+
+var catalogEntryTen = new ImageProducts('dragon', 'Dragon Meat', 'assets/dragon.jpg');
+
+var catalogEntryEleven = new ImageProducts('pen','Pen Utensil Covers', 'assets/pen.jpg');
+
+var catalogEntryTwelve = new ImageProducts('pet-sweep', 'Pet Foot Mops','assets/pet-sweep.jpg');
+
+var catalogEntryThirteen = new ImageProducts('scissors','Perfect Pizza Cutter', 'assets/scissors.jpg');
+
+var catalogEntryFourteen = new ImageProducts('shark', 'Shark Sleeping Bag','assets/shark.jpg');
+
+var catalogEntryFifteen = new ImageProducts('sweep', 'Baby Mop Onesie','assets/sweep.png');
+
+var catalogEntrySixteen = new ImageProducts('tauntaun', 'Tauntaun Sleeping Bag','assets/tauntaun');
+
+var catalogEntrySeventeen = new ImageProducts('unicorn', 'Unicorn Meat','assets/unicorn.jpg');
+
+var catalogEntryEighteen = new ImageProducts('usb','Tentacle USB Drive','assets/usb.gif');
+
+var catalogEntryNineteen = new ImageProducts('water-can', 'Ridiculous Watering Can', 'assets/water-can.jpg');
+
+var catalogEntryTwenty = new ImageProducts('wine-glass', 'Death Star Wine Glass', 'assets/wine-glass.jpg');
+
+for (var i = 0; i < productImages.length; i++) {
+  var el = document.getElementById('firstImage');
+  setAttribute
+
+}
